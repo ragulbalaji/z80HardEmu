1b087faa2e29525e942cabfae3b6b753b09dfde7
Ho Jie Feng <hjf3108@gmail.com>
2019-01-20 09:41:58 +0800

    add state parsing

--- ServerNode/server.js
+++ ServerNode/server.js
@@ -5,7 +5,7 @@
 
 // Emulation Block
 let CpuMemory = new Array(0x10000)
-CpuMemory.fill(0x0)
+CpuMemory.fill(0xC5)
 
 /*
 // Web Sockets Block
@@ -24,28 +24,28 @@ const Readline = require('@serialport/parser-readline')
 let EmulatorSerial, EmulatorRawSerial
 
 var rom = [
-  0x01,0x00,0x01,0xed,0x43,0x00,0x01,0x03,0xc3,0x03,0x00
+  //0x01,0x00,0x01,0xed,0x43,0x00,0x01,0x03,0xc3,0x03,0x00
 
   //0x01, 0xff, 0xff, 0xc5, 0xc3, 0x06, 0x00, 0x00, 0x00
-  
-  //0x31, 0x00, 0x01, // LD SP, 0x0100
-  //0x01, 0x00, 0x00, // LD BC,0x0
-  //0x21, 0x00, 0x10, // LD HL, 0x1000
+  //0xC5, 0xC5
+  /*0x31, 0x00, 0x01, // LD SP, 0x0100
+  0x01, 0x00, 0x00, // LD BC,0x0
+  0x21, 0x00, 0x10, // LD HL, 0x1000
   // LOOP:
-  //0xC5, // PUSH BC
-  //0x03, // INC BC
-  //0x7E, // LD   A,(HL)
-  //0x3C, // INC   A
-  //0x77, // LD   (HL),A
-  //0xC3, 0x06, 0x00 // JP LOOP
+  0xC5, // PUSH BC
+  0x03, // INC BC
+  0x7E, // LD   A,(HL)
+  0x3C, // INC   A
+  0x77, // LD   (HL),A
+  0xC3, 0x06, 0x00 // JP LOOP*/
 ]
-
+//rom = ["2".charCodeAt(0), "A".charCodeAt(0), "B".charCodeAt(0)]
 for (var i = 0; i < rom.length; i++) {
   CpuMemory[i] = rom[i]
 }
 try {
-  // EmulatorRawSerial = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
-  EmulatorRawSerial = new SerialPort('/dev/tty.usbmodem14101', { baudRate: 115200 })
+  EmulatorRawSerial = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
+  //EmulatorRawSerial = new SerialPort('/dev/tty.usbmodem14101', { baudRate: 115200 })
   EmulatorSerial = EmulatorRawSerial.pipe(new Readline({ delimiter: '\n' }))
 
   EmulatorSerial.on('data', function (data) {
@@ -67,6 +67,10 @@ try {
         CpuMemory[addr] = d0
         console.log('Write', '0x' + addr.toString(16), ':', '0x' + d0.toString(16))
         // console.log('W', addr.toString(16), CpuMemory[addr].toString(16), d0)
+        break
+      case 'S':
+        cmds.shift();
+        states = cmds.map(num => parseInt(num, 10))
         break
       default:
         console.error('❌ Resetting CPU :', data)
